3ae8cd4e8e5c481dff812def5b8068969ec71b3b
Illia Aprol <asdf21072107@gmail.com>
2022-02-07 22:48:16 +0300

    Add Cumpuss.Presentation

---
+++ back/Cumpuss.Presentation/Controllers/StudentController.cs
@@ -0,0 +1,14 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace Cumpuss.Presentation.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class StudentController : ControllerBase
+    {
+        public StudentController()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
